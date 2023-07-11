f38b03627538865fbbe3d733552e46447a238c76
Marcos Pablo <105401301+marcosspablo@users.noreply.github.com>
2023-07-11 19:53:35 -0300

    Base do projeto

---
+++ src/main/java/com/projeto/game/projetogame/ProjetoGameApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.game.projetogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/game/projetogame/ProjetoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.game.projetogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
