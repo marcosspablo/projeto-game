6412e03f3b6e9032faf3948767ae8d629e9af3a4
Marcos Pablo <105401301+marcosspablo@users.noreply.github.com>
2023-07-11 20:07:33 -0300

    Criação tabela de jogos

--- src/main/java/com/projeto/game/projetogame/entities/Game.java
+++ src/main/java/com/projeto/game/projetogame/entities/Game.java
@@ -14,24 +14,28 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private  String genre;
-    private  String platform;
+    private  String platforms;
+    private  Double score;
     private  String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-    private  String longDescripition;
+    @Column(columnDefinition = "TEXT")
+    private  String longDescription;
 
     public Game() {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescripition) {
+    public Game(Long id, String title, Integer year, String genre, String platforms,  Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
-        this.longDescripition = longDescripition;
+        this.longDescription = longDescription;
     }
 
     public Long getId() {
@@ -66,12 +70,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platform) {
+        this.platforms = platform;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -90,12 +102,12 @@ public class Game {
         this.shortDescription = shortDescription;
     }
 
-    public String getLongDescripition() {
-        return longDescripition;
+    public String getLongDescription() {
+        return longDescription;
     }
 
-    public void setLongDescripition(String longDescripition) {
-        this.longDescripition = longDescripition;
+    public void setLongDescription(String longDescripition) {
+        this.longDescription = longDescripition;
     }
 
     @Override
